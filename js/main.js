e8de1919a8e33c582031731d39826690e2425fd4
Haley Billson <h_billson@fanshaweonline.ca>
2020-01-24 10:43:47 -0500

    changed up a couple things

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 (() => {
   console.log('hello from JS!');
 
-  let allSVGs = document.querySelectorAll("img");
+  let allIMGs = document.querySelectorAll("img"),
+  allSVGs = document.querySelectorAll("svg");
 
   function logSVG() {
     console.log(this.id);
   }
 
+  allIMGs.forEach(item => item.addEventListener("click", logSVG));
   allSVGs.forEach(item => item.addEventListener("click", logSVG));
 
 // document.querySelector('h1').textContent = "What is up? I am script";
